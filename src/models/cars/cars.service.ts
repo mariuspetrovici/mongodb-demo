d7437d8fd4aa192ed25b8d77103a7742d072f0e9
Marius Petrovici <marius.petrovici@assist.ro>
2023-04-19 23:48:32 +0300

    exercise 1

--- src/models/cars/cars.service.ts
+++ src/models/cars/cars.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -14,6 +18,16 @@ export class CarsService {
     average_horsepower: number,
     average_price?: number,
   ) {
+    /* -- EXERCISE 1 -- */
+    const existingRecord = await this.carModel.findOne({
+      name: new RegExp(name, 'i'),
+    });
+
+    if (existingRecord) {
+      throw new ConflictException('Car name already exists');
+    }
+    /* -------------- */
+
     const newCar = new this.carModel({
       name,
       nr_of_models,
